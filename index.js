638313ae00cd66105e77dcd51f2bb5b7fcdda111
alibeknakhimov <mega.nahimovs5@gmail.com>
2024-02-28 15:03:41 +0600

    f

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ var vosk = require('vosk');
 var fs = require("fs");
 var mic = require("mic");
 var levenshtein = require('js-levenshtein');
-
+const player = require('play-sound')();
+const audioFilePaths = ['audio/greet1.wav', 'audio/greet2.wav', 'audio/greet3.wav'];
+const doneAudioFilePaths = ['audio/ok1.wav', 'audio/ok2.wav', 'audio/ok3.wav'];
+const thanksAudioFilePath = ['audio/thanks.wav'];
 const MODEL_PATH = "vosk_rus";
 const SAMPLE_RATE = 16000;
 const REFERENCE_COMMAND = "открой дверь";
@@ -28,6 +31,15 @@ async function startWakeWordDetection() {
         const keywordIndex = porcupine.process(frames);
         if (keywordIndex !== -1) {
             console.log("Ключевое слово 'Jarvis' обнаружено. Активация голосового ассистента...");
+            const randomIndex = Math.floor(Math.random() * audioFilePaths.length);
+            const selectedAudioFilePath = audioFilePaths[randomIndex];
+            player.play(selectedAudioFilePath, function(err){
+                if (err) {
+                  console.error('Ошибка воспроизведения:', err);
+                } else {
+                  console.log('Аудиофайл успешно воспроизведен');
+                }
+              });
             isListeningForWakeWord = false;
             startCommandRecognition();
         }
@@ -69,13 +81,36 @@ async function startCommandRecognition() {
             const result = rec.result();
             const recognizedCommand = result.text.toLowerCase();
             const distance = levenshtein(REFERENCE_COMMAND, recognizedCommand);
+            const distanceThanks = levenshtein('спасибо', recognizedCommand);
             const similarity = 1 - distance / Math.max(REFERENCE_COMMAND.length, recognizedCommand.length);
-            
+            const similarityThanks = 1 - distanceThanks / Math.max('спасибо'.length, recognizedCommand.length);
             console.log(`Совпадение с командой: ${similarity.toFixed(2)}`);
-            
+            console.log(`Совпадение с командой "спасибо": ${similarityThanks.toFixed(2)}`);
             if (similarity >= COMMAND_THRESHOLD) {
                 console.log(`Команда распознана: ${REFERENCE_COMMAND}`);
+                const doneRandomIndex = Math.floor(Math.random() * doneAudioFilePaths.length);
+                const doneSelectedAudioFilePath = doneAudioFilePaths[doneRandomIndex];
                 sendMessage('alibek');
+                player.play(doneSelectedAudioFilePath, function(err){
+                if (err) {
+                  console.error('Ошибка воспроизведения:', err);
+                } else {
+                  console.log('Аудиофайл успешно воспроизведен');
+                }
+                });
+                micInstance.stop();
+                startWakeWordDetection();
+            }
+            else if (similarityThanks >= COMMAND_THRESHOLD) {
+                console.log('Команда распознана: спасибо');
+                console.log('Всегда к вашим услугам, сэр');
+                player.play(thanksAudioFilePath, function(err){
+                if (err) {
+                    console.error('Ошибка воспроизведения:', err);
+                } else {
+                    console.log('Аудиофайл успешно воспроизведен');
+                }
+                });
                 micInstance.stop();
                 startWakeWordDetection();
             }
