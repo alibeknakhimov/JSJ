9d769e2fc12b2aac63c58160a8ec0216a7c04e42
alibeknakhimov <mega.nakhimovs5@gmail.com>
2024-03-28 01:06:59 +0500

    f

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var fs = require("fs");
 var mic = require("mic");
 var levenshtein = require('js-levenshtein');
 const player = require('play-sound')();
+const { exec } = require('child_process');
 const audioFilePaths = ['audio/greet1.wav', 'audio/greet2.wav', 'audio/greet3.wav'];
 const doneAudioFilePaths = ['audio/ok1.wav', 'audio/ok2.wav', 'audio/ok3.wav'];
 const thanksAudioFilePath = ['audio/thanks.wav'];
@@ -17,9 +18,9 @@ const COMMAND_THRESHOLD = 0.70;
 let isListeningForWakeWord = false;
 
 const porcupine = new Porcupine(
-    "1VdXMAPnLWa62EV5fPnCs4NR+052DBScPbuA7OjptSAP/IB2KEDfKQ==",
+    "XRpmKl8diU1EIuplShvBmmyNAKvErLRMewM0FFxKL5zv/DfKmrmjAQ==",
     ["Jarvis_en_linux_v3_0_0.ppn"],
-    [1.0] // Устанавливаем порог для обнаружения ключевого слова
+    [0.775] // Устанавливаем порог для обнаружения ключевого слова
 );
 const recorder = new PvRecorder(512);
 
@@ -80,13 +81,29 @@ async function startCommandRecognition() {
         if (rec.acceptWaveform(data)) {
             const result = rec.result();
             const recognizedCommand = result.text.toLowerCase();
-            const distance = levenshtein(REFERENCE_COMMAND, recognizedCommand);
+            const distanceOpenDoor = levenshtein('открой дверь', recognizedCommand);
+            const distanceAboutMe = levenshtein('расскажи о себе', recognizedCommand);
+            const distanceOpenBrowser = levenshtein('открой браузер', recognizedCommand);
+            const distancePlayMusic = levenshtein('включи музыку', recognizedCommand);
+            const distanceSendMessage = levenshtein('отправь сообщение', recognizedCommand);
+            const distanceShutdownComputer = levenshtein('выключи компьютер', recognizedCommand);
             const distanceThanks = levenshtein('спасибо', recognizedCommand);
-            const similarity = 1 - distance / Math.max(REFERENCE_COMMAND.length, recognizedCommand.length);
+            const similarityOpenDoor = 1 - distanceOpenDoor / Math.max('открой дверь'.length, recognizedCommand.length);
+            const similarityAboutMe = 1 - distanceAboutMe / Math.max('расскажи о себе'.length, recognizedCommand.length);
+            const similarityOpenBrowser = 1 - distanceOpenBrowser / Math.max('открой браузер'.length, recognizedCommand.length);
+            const similarityPlayMusic = 1 - distancePlayMusic / Math.max('включи музыку'.length, recognizedCommand.length);
+            const similaritySendMessage = 1 - distanceSendMessage / Math.max('отправь сообщение'.length, recognizedCommand.length);
+            const similarityShutdownComputer = 1 - distanceShutdownComputer / Math.max('выключи компьютер'.length, recognizedCommand.length);
             const similarityThanks = 1 - distanceThanks / Math.max('спасибо'.length, recognizedCommand.length);
-            console.log(`Совпадение с командой: ${similarity.toFixed(2)}`);
+            console.log(`Совпадение с командой "открой дверь": ${similarityOpenDoor.toFixed(2)}`);
+            console.log(`Совпадение с командой "расскажи о себе": ${similarityAboutMe.toFixed(2)}`);
+            console.log(`Совпадение с командой "открой браузер": ${similarityOpenBrowser.toFixed(2)}`);
+            console.log(`Совпадение с командой "включи музыку": ${similarityPlayMusic.toFixed(2)}`);
+            console.log(`Совпадение с командой "отправь сообщение": ${similaritySendMessage.toFixed(2)}`);
+            console.log(`Совпадение с командой "выключи компьютер": ${similarityShutdownComputer.toFixed(2)}`);
             console.log(`Совпадение с командой "спасибо": ${similarityThanks.toFixed(2)}`);
-            if (similarity >= COMMAND_THRESHOLD) {
+
+            if (similarityOpenDoor >= COMMAND_THRESHOLD) {
                 console.log(`Команда распознана: ${REFERENCE_COMMAND}`);
                 const doneRandomIndex = Math.floor(Math.random() * doneAudioFilePaths.length);
                 const doneSelectedAudioFilePath = doneAudioFilePaths[doneRandomIndex];
@@ -113,6 +130,28 @@ async function startCommandRecognition() {
                 });
                 micInstance.stop();
                 startWakeWordDetection();
+            }
+            else if (similarityOpenBrowser >= COMMAND_THRESHOLD){
+                const doneRandomIndex = Math.floor(Math.random() * doneAudioFilePaths.length);
+                const doneSelectedAudioFilePath = doneAudioFilePaths[doneRandomIndex];
+                player.play(doneSelectedAudioFilePath, function(err){
+                    if (err) {
+                      console.error('Ошибка воспроизведения:', err);
+                    } else {
+                      console.log('Аудиофайл успешно воспроизведен');
+                    }
+                    });
+                    micInstance.stop();
+                    startWakeWordDetection();
+                url = 'https://www.youtube.com';
+                exec(`xdg-open ${url}`, (error, stdout, stderr) => {
+                    if (error) {
+                      console.error(`Ошибка при открытии браузера: ${error.message}`);
+                      return;
+                    }
+                    console.log(`Стандартный вывод: ${stdout}`);
+                    console.error(`Стандартный вывод ошибки: ${stderr}`);
+                  });
             }
         }
     });
